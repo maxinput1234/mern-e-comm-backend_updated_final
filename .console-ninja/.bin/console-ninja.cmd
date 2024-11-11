438bffc81336f55ef289c8dceddec3378879c24c
Mayank Suthar <sutharmayank001@gmail.com>
2024-11-11 23:35:34 +0530

    create pages and routes

---
+++ .console-ninja/.bin/console-ninja.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF
+set NODE_OPTIONS=--no-warnings --require "C:/Users/DELL/.console-ninja/.bin/loader.js"
+%*
---
+++ .console-ninja/.bin/loader.js
@@ -0,0 +1,1 @@
+"use strict";try{let{createRequire:e}=require("module");e("file:///c:/Users/DELL/.vscode/extensions/wallabyjs.console-ninja-1.0.364/out/buildHook/")("./index.js").default({tool:"node"})}catch(e){console.log(e)}
